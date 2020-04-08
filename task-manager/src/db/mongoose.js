1948789c16ad25028bb72c6fc0ce56a87ba43925
Dan <12995058+danielhac@users.noreply.github.com>
2020-04-07 19:17:04 -0700

    connected mongodb cloud

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const secret = require('./secret')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect('mongodb+srv://dan:' + secret + '@nodejs-cluster-esv7d.mongodb.net/task-manager?retryWrites=true&w=majority', {
+// mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
