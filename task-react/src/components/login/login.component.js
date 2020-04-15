e4fbc719df1e264ad77e735842c661af8c562729
Dan <12995058+danielhac@users.noreply.github.com>
2020-04-14 23:39:02 -0700

    Added error handling for user logins

--- task-react/src/components/login/login.component.js
+++ task-react/src/components/login/login.component.js
@@ -1,11 +1,16 @@
+/*
+  *   This component dislays the email/password input fields .
+  */
+
 import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 
-export const Login = ({handleSubmit, emailChange, passChange}) => {
+export const Login = ({handleSubmit, emailChange, passChange, checkLoginError}) => {
     return (
         <form className='login' onSubmit={handleSubmit}>
             <Container>
@@ -30,6 +35,9 @@ export const Login = ({handleSubmit, emailChange, passChange}) => {
                         <Button type='submit' value='Login' variant="success">Login</Button>
                     </Col>
                 </Row>
+                {checkLoginError && <Row className="justify-content-md-center login-error">        
+                    <Alert variant='danger'>{checkLoginError}</Alert>
+                </Row>}
             </Container>
         </form>
     )
