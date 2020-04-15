25da8ac53864a3bb8a9f1656048be508c5acd4d9
Dan <12995058+danielhac@users.noreply.github.com>
2020-04-14 20:08:02 -0700

    Updated to call the server directly, handled credential errors, added comments

--- task-react/src/App.js
+++ task-react/src/App.js
@@ -1,3 +1,10 @@
+/*  
+*   A to-do task list client-side application built with React.
+*     - Uses a token issued after account creation or login to interact with 
+*       the server-side REST API.
+*/
+
+
 import React, { Component } from 'react';
 import { CreateUser } from './components/create-user/create-user.component';
 import { Login } from './components/login/login.component'
@@ -5,7 +12,10 @@ import { Logout } from './components/logout/logout.component'
 import { TaskList } from './components/task-list/task-list.component'
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './App.css';
-const secret = require('./secret')
+import secret from './secret'
+
+// const url = 'http://localhost:3001'
+const url = 'http://taskappmgr-env.eba-tdqrarch.us-west-2.elasticbeanstalk.com'
 
 
 class App extends Component {
@@ -19,15 +29,16 @@ class App extends Component {
       email: '',
       token: '',
       newtask: '',
-      loginerror: null
+      newtaskcompleted: false,
+      loginerror: '',
+      createerror: ''
     }
   }
 
-  /* NOTES
+  /* NOTES for local development
   *
   * Ports
   * 3000 - Client react
-  * 3001 - Client server
   * 3002 - Server side
   */
 
@@ -45,27 +56,29 @@ class App extends Component {
     // }
   // }
 
-  
+
+  /*
+  *   Functions used in components that set state variables
+  */
   emailChange = (e) => {
-    console.log(e.target.value);
     this.setState({ email: e.target.value})
   }
 
   userChange = (e) => {
-    console.log(e.target.value);
     this.setState({ username: e.target.value})
   }
 
   passChange = (e) => {
-    console.log(e.target.value);
     this.setState({ password: e.target.value})
   }
 
+  /*
+  *   Creates a new user
+  *     - Takes user info from state to create the user by calling the API
+  *     - Creates a new task upon account creation and displays it
+  */
   handleCreateUserSubmit = (e) => {
     e.preventDefault()
-    console.log("email found: " + this.state.email);
-    console.log("password found: " + this.state.password);
-    console.log("username found: " + this.state.username);
 
     const credentials =  JSON.stringify({
       "email": this.state.email,
@@ -73,25 +86,33 @@ class App extends Component {
       "name": this.state.username
     })
 
-    // TODO: handle when credentials are incorrect
-    fetch('http://localhost:3001/users', {
+    fetch(url + '/users', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'credentials': credentials
-      }
+        // 'credentials': credentials
+      },
+      body: credentials
     })
     .then(response => response.json())
-    .then((jwt) => this.setState({ token: jwt.token, newtask: "Create your first task by modifying this text or Add new task blow!"}))
+    .then((jwt) => {
+      if(!jwt.token) {
+        this.setState({ createerror: 'E-mail must be in correct format. Password must be at least 7 characters.'})
+        throw new Error('E-mail must be in correct format. Password must be at least 7 characters.')
+      }
+      this.setState({ token: jwt.token, createerror: '', newtask: "Create your first task by modifying this text or Add new task below!"})
+    })
     .then(() => this.handleTaskAdd())
     .then(() => this.getTasks())
     .catch(err => console.error(err))
   }
 
+  /*
+  *   Login into account
+  *     - Takes user's credentials from state and calls the API
+  *     - Displays user's tasks upon successful login
+  */
   handleSubmit = (e) => {
-    // console.log("email found: " + this.state.email);
-    // console.log("password found: " + this.state.password);
-
     // For prod use
     const credentials =  JSON.stringify({
       "email": this.state.email,
@@ -101,24 +122,34 @@ class App extends Component {
     // For quick local use
     // const credentials = secret
 
-
-    // TODO: handle when credentials are incorrect
-    fetch('http://localhost:3001/login', {
+    fetch(url + '/users/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'credentials': credentials
-      }
+        // 'credentials': credentials
+      },
+      body: credentials
     })
     .then(response => response.json())
-    .then((jwt) => this.setState({ token: jwt.token}))
+    .then((jwt) => {
+      if(!jwt.token) {
+        this.setState({ loginerror: 'The E-mail and Password combination cannot be found! Please try again or Create an Account.'})
+        throw new Error('The E-mail and Password combination cannot be found! Please try again or Create an Account.')
+      }
+      this.setState({ token: jwt.token, loginerror: ''})
+    })
     .then(() => this.getTasks())
-    .catch(err => console.error(err))
+    .catch(err => {
+      console.error(err)
+    })
     e.preventDefault()
   }
 
+  /*
+  *     Gets users tasks by updating state
+  */
   getTasks() {
-    fetch('http://localhost:3001/tasks', {
+    fetch(url + '/tasks', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -131,10 +162,14 @@ class App extends Component {
     
   }
 
+  /*
+  *   Logout the user by calling the API
+  *     - resets state
+  */
   handleLogout = (e) => {
     console.log(this.state.token)
 
-    fetch('http://localhost:3001/logout', {
+    fetch(url + '/users/logout', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -147,26 +182,39 @@ class App extends Component {
       tasks: [],
       username: '',
       password: '',
-      email: ''
+      email: '',
+      newtask: ''
     }))
     .catch(err => console.error(err))
     e.preventDefault()
   }
 
+  /*
+  *   Functions used in components that set state variables
+  *     - For adding new tasks
+  */
   changeTaskAdd = (e) => {
-    console.log(e.target.value);
     this.setState({ newtask: e.target.value})
   }
 
+  changeCompletedAdd = (e) => {
+    this.setState({ newtaskcompleted: e.target.checked})
+  }
+  
+  /*
+  *   Add a new task
+  *     - takes state variables and calls the API
+  *     - update state tasks afterwards
+  */
   handleTaskAdd = (e) => {
     if(e) e.preventDefault()
-    console.log('handleTaskAdd', this.state.newtask);
 
     const body = JSON.stringify({
-      'description': this.state.newtask
+      'description': this.state.newtask,
+      'completed': this.state.newtaskcompleted
     })
     
-    fetch('http://localhost:3001/tasks/', {
+    fetch(url + '/tasks/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -180,20 +228,24 @@ class App extends Component {
       "description": result.description,
       "completed": result.completed
     })}))
+    .then(() => this.setState({newtaskcompleted: false}))
     .catch(err => console.error(err))
     
     
   }
 
+  /*
+  *   Modify tasks and status of completion
+  *     - onchange function within component
+  *     @e contains either task description or boolean completion
+  *     - calls API with updating task info
+  *   NOTE: improve by implementing a submit for modifications and call the API just once instead
+  */
   handleTaskCompleted = (e) => {
-    // console.log('in handleAddTask ', e.target);
-    // console.log('in handleAddTask ', e.target.checked, e.target.name);
-    
     let body = ''
     
     // Check if the value coming in from 'e' is the checkbox or description
     const taskVal = e.target.name == 'description' ? e.target.value : e.target.checked
-    console.log(taskVal);
     
     if(typeof taskVal === 'string') {
       body = JSON.stringify({
@@ -205,7 +257,7 @@ class App extends Component {
       })
     }
 
-    fetch('http://localhost:3001/tasks/' + e.target.id, {
+    fetch(url + '/tasks/' + e.target.id, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
@@ -217,11 +269,16 @@ class App extends Component {
     .catch(err => console.error(err))
   }
 
+  /*
+  *   Delete a user's task
+  *     @e contains the 'id' to use to call the API to delete the task
+  *     - update state for tasks
+  */
   handleTaskDelete = (e) => {
-    console.log('handleTaskDelete', e.target.id);
+    // console.log('handleTaskDelete', e.target.id);
     const delTaskId = e.target.id
 
-    fetch('http://localhost:3001/tasks/' + e.target.id, {
+    fetch(url + '/tasks/' + e.target.id, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
@@ -230,7 +287,6 @@ class App extends Component {
     })
     .then(() => {
       let arr = this.state.tasks
-      // console.log(arr.length);
       for(let i=0; i<arr.length; i++) {
         if(arr[i]._id == delTaskId) {
           arr.splice(i, 1)
@@ -242,19 +298,28 @@ class App extends Component {
     .catch(err => console.error(err))
   }
 
+  /*
+  *   Renders the components
+  *     If not logged in, display components
+  *       - Login
+  *       - Create User
+  *     If logged in, display components
+  *       - Logout
+  *       - Tasks
+  */
   render() {
 
     return (
       <div className='App'>
-        {!this.state.token && <Login emailChange={this.emailChange} passChange={this.passChange} handleSubmit={this.handleSubmit}></Login>}
+        {!this.state.token && <Login emailChange={this.emailChange} passChange={this.passChange} handleSubmit={this.handleSubmit} checkLoginError={this.state.loginerror}></Login>}
+
         <Jumbotron>
           <h1>Task App</h1>
           <p>Create, read, update, delete your tasks!</p>
-          {!this.state.token && <CreateUser handleCreateUserSubmit={this.handleCreateUserSubmit} userChange={this.userChange} emailChange={this.emailChange} passChange={this.passChange}></CreateUser>}
+          {!this.state.token && <CreateUser handleCreateUserSubmit={this.handleCreateUserSubmit} userChange={this.userChange} emailChange={this.emailChange} passChange={this.passChange} checkCreateError={this.state.createerror}></CreateUser>}
           {this.state.token && <Logout handleLogout={this.handleLogout}></Logout>}
         </Jumbotron>
-
-        {this.state.token && <TaskList tasks={this.state.tasks} handleTaskCompleted={this.handleTaskCompleted} handleTaskDelete={this.handleTaskDelete} changeTaskAdd={this.changeTaskAdd} handleTaskAdd={this.handleTaskAdd}></TaskList>}
+        {this.state.token && <TaskList tasks={this.state.tasks} handleTaskCompleted={this.handleTaskCompleted} handleTaskDelete={this.handleTaskDelete} changeTaskAdd={this.changeTaskAdd} changeCompletedAdd={this.changeCompletedAdd} handleTaskAdd={this.handleTaskAdd}></TaskList>}
     </div>
     )
   }
