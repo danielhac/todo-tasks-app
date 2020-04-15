84ba722721096778571ade4610b652688c290206
Dan <12995058+danielhac@users.noreply.github.com>
2020-04-14 23:29:46 -0700

    Enabled a new task to be checked as completed

--- task-react/src/components/add-task/add-task.component.js
+++ task-react/src/components/add-task/add-task.component.js
@@ -1,8 +1,15 @@
+  /*
+  *   This component displays the task's description and completed input fields to add a task.
+  */
+
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 
-export const AddTask = ({changeTaskAdd, handleTaskAdd}) => {
-    const handleClearField = () => document.getElementsByClassName('add-description')[0].value = ''
+export const AddTask = ({changeTaskAdd, handleTaskAdd, changeCompletedAdd}) => {
+    const handleClearField = () => { 
+        document.getElementsByClassName('add-description')[0].value = ''
+        document.getElementsByClassName('add-completed')[0].checked = false
+    }
     const handleAddAndClear = () => {
         handleTaskAdd()
         handleClearField()
@@ -11,7 +18,7 @@ export const AddTask = ({changeTaskAdd, handleTaskAdd}) => {
     return (
         <tr>
             <td><input name="description" type="text" className="add-description" onChange={changeTaskAdd}></input></td>
-            <td><input name="completed" type="checkbox"/></td>
+            <td><input name="completed" type="checkbox" className="add-completed" onChange={changeCompletedAdd} /></td>
             <td><Button type="submit" variant="outline-success" onClick={handleAddAndClear}>Add Task</Button></td>
         </tr>
 
